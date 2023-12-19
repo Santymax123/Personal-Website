a698d9ce0472885fd25f18a004e99510fdae4c17
Max <Santymax123@hotmail.com>
2023-12-19 18:31:46 +0800

    added colour theme and navigation buttons

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('nav-button').addEventListener('mouseover', function () {
+    this.style.backgroundColor = '#6d6f1b';
+});
+
+document.getElementById('nav-button').addEventListener('mouseout', function () {
+    this.style.backgroundColor = '#babf65';
+});
